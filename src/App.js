f46ee8e619756fe80ef6e9bed2b0e43d74e7bb31
Joon Yoon <myexistance@hotmail.com>
2019-03-30 11:01:11 +0000

    no errors

--- src/App.js
+++ src/App.js
@@ -2,27 +2,49 @@ import React, { Component } from 'react';
 import $ from 'jquery';
 import './App.css';
 
-
-export function queryBehance(query) {
- var term = query.replace(/\s/, "+");
- var url = "https://api.behance.net/v2/projects?q=motorcycle&client_id=dSoSYYcQQTsMHJ9O5hWkN6gns4aV5gOM" + term;
-
 class App extends Component {
-  render() {
+  constructor(props){
+    super(props);
+    this.state = {
+      projects:[]
+    }
+  }
 
-    return $.ajax({
+getprojects(){
+  const url = "https://api.behance.net/v2/projects?q=motorcycle&client_id=dSoSYYcQQTsMHJ9O5hWkN6gns4aV5gOM"
+
+  $.ajax({
     url: url,
     type: "get",
     data: {projects: {}},
-    dataType: "jsonp",
+    dataType: "jsonp"
   }).done((response) => {
-    return <div className="App">response["projects"]
-    </div>
-  }).fail(() => {
+    this.setState({
+      portfolioData: response['projects']
+    });
+  }).fail((error) => {
     console.log("Ajax request fails")
-  })
+    console.log(error);
+  });
 }
 
-  }
-}
+
+ render() {
+     return (
+       <div className="App">
+         <div className="row">
+           <div>
+             {this.projects}
+           </div>
+         </div>
+
+       </div>
+     );
+   }
+ }
+
+
+
+
+
 export default App;
